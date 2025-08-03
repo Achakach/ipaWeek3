65bdc09a3da38ecdfb32d446cd2a772240c64bb5
Achakach <66070244@kmitl.ac.th>
2025-08-03 00:23:56 +0000

    checkpoint b4 add an extra

---
+++ netmiko/r2.py
@@ -0,0 +1,53 @@
+from netmiko import ConnectHandler
+import os
+
+device_ip = "172.31.144.5"
+username = "admin"
+key_path = os.path.expanduser("~/.ssh/id_rsa.pub")
+
+device_params = {
+    'device_type': 'cisco_ios',
+    'ip': device_ip,
+    'username': username,
+    'key_file' : key_path,
+    'use_keys': True,
+}
+
+with ConnectHandler(**device_params) as ssh:
+    result = ssh.enable()
+    print(result)
+    result = ssh.config_mode()
+    print(result)
+    result = ssh.send_command_timing("router ospf 1 vrf control-data")
+    print(result)
+    result = ssh.send_command_timing("network 172.31.144.0 0.0.0.255 area 0")
+    print(result)
+    result = ssh.send_command_timing("network 192.168.122.0 0.0.0.255 area 0")
+    print(result)
+    result = ssh.send_command_timing("default-information originate always")
+    print(result)
+    result = ssh.send_command_timing("exit")
+    print(result)
+    result = ssh.send_command_timing("int g0/1")
+    print(result)
+    result = ssh.send_command_timing("ip vrf forwarding control-data")
+    print(result)
+    result = ssh.send_command_timing("ip add 172.31.144.7 255.255.255.240")
+    print(result)
+    result = ssh.send_command_timing("no shut")
+    print(result)
+    result = ssh.send_command_timing("ip ospf 1 area 0")
+    print(result)
+    result = ssh.send_command_timing("exit")
+    print(result)
+    result = ssh.send_command_timing("int loopback0")
+    print(result)
+    result = ssh.send_command_timing("ip vrf forwarding control-data")
+    print(result)
+    result = ssh.send_command_timing("ip add 1.1.1.2 255.255.255.0")
+    print(result)
+    result = ssh.send_command_timing("no shut")
+    print(result)
+    result = ssh.send_command_timing("ip ospf 1 area 0")
+    print(result)
+    ssh.disconnect()
